aa3da47c13a9bc35ecddd1aabaaa19117aeffb65
jtrtj <jt.r@live.com>
2018-09-28 16:44:05 -0600

    adding edge cases to tests

--- mergeSort.js
+++ mergeSort.js
@@ -1,9 +1,35 @@
-module.exports = {
-  merge: function(firstSplit, remainingSplit) {
-    return "test";
-  },
-  mergeSort: function(array) {
-    return "aye yo";
-  }
+function merge(lSplit, rSplit) {
+  let merged = [];
+  let lLength = lSplit.length
+  let rLength = rSplit.length
+  let lIndex = 0;
+  let rIndex = 0;
+
+  while (lIndex < lLength && rIndex < rLength) {
+    if (lSplit[lIndex] < rSplit[rIndex]) {
+      merged.push(lSplit[lIndex]);
+      lIndex += 1;
+    } else {
+      merged.push(rSplit[rIndex]);
+      rIndex += 1;
+    };
+  };
+
+  return merged.concat(lSplit.slice(lIndex)).concat(rSplit.slice(rIndex));
 };
-   
+
+function mergeSort(list) {
+  const length = list.length
+  if (length < 2) {
+    return list;
+  } 
+  const middle = Math.floor(list.length / 2);
+  const lSplit = list.slice(0, middle);
+  const rSplit = list.slice(middle);
+
+  return merge(mergeSort(lSplit), mergeSort(rSplit))
+};
+
+// const list = [2,0,1,3,6,3,23,677]
+// console.log(mergeSort(list))
+module.exports = {merge,mergeSort}
--- test/mergeSort-test.js
+++ test/mergeSort-test.js
@@ -1,10 +1,17 @@
 const assert = require('chai').assert;
-const mergeSort = require('../mergeSort');
+const sorter = require('../mergeSort');
 
 describe('mergeSort functionality', function() {
   context('merge function', function() {
-    it('it can compare two arrays values and return sorted merged array', function() {
-      assert.deepEqual(mergeSort.merge([2,0],[3,1]), [0,1,2,3])
+    it('it can merge two sorted arrays', function() {
+      assert.deepEqual(sorter.merge([0,5],[1,3]), [0,1,3,5])
+      assert.deepEqual(sorter.merge([0],[1]), [0,1])
+    })
+  })
+
+  context('mergeSort function', function() {
+    it('it can merge sort a list of values', function() {
+      assert.deepEqual(sorter.mergeSort([2,0,1,3]), [0,1,2,3])
     })
   })
 })
